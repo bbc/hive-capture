acd37ceb2325bdcb6cceb81e434375a9c809d47d
Joe Haig <joe.haig@bbc.co.uk>
2015-04-29 11:01:23 +0100

    Graph polling delays
    These graphs are not perfect as they do not combine the data collected by
    each thread.

--- lib/hive_capture/data_store.rb
+++ lib/hive_capture/data_store.rb
@@ -1,9 +1,12 @@
+require 'gruff'
+
 class HiveCapture
   module DataStore
     @devices = {}
 
     def self.poll_delay(id, delay)
       self.device(id)[:delay] = delay
+      self.device(id)[:delay_history][Time.new] = delay
     end
 
     def self.get_poll_delays
@@ -14,8 +17,41 @@ class HiveCapture
       delays
     end
 
+    def self.get_poll_history
+      cutoff = Time.new - 30 * 60
+      delays = {}
+      image = Gruff::Line.new
+      image.title = 'Polling response times'
+      image.labels = {
+        cutoff.to_i => '0',
+        cutoff.to_i + 5 * 60 => '5',
+        cutoff.to_i + 10 * 60 => '10',
+        cutoff.to_i + 15 * 60 => '15',
+        cutoff.to_i + 20 * 60 => '20',
+        cutoff.to_i + 25 * 60 => '25',
+        cutoff.to_i + 30 * 60 => '30',
+      }
+      @devices.collect do |n, vals|
+        delays[n] = []
+        vals[:delay_history].each do |t, d|
+          if t < cutoff
+             vals.delete(t)
+          else
+            delays[n] << [t.to_i, d]
+          end
+        end
+        delays[n].each do |thing|
+          p thing
+        end
+        delays[n].sort! { |a, b| a[0] <=> b[0] }
+        image.dataxy(n.to_s, delays[n])
+      end
+      image.write('/home/vmuser/titantv/public/delays.png')
+      delays
+    end
+
     def self.device(id)
-      (@devices[id] = {} if ! @devices.has_key?(id)) || @devices[id]
+      (@devices[id] = { delay_history: {} } if ! @devices.has_key?(id)) || @devices[id]
     end
   end
 end
