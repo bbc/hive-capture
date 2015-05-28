6731728d09fa09352c334b0960920f829cef2529
Joe Haig <joe.haig@bbc.co.uk>
2015-05-28 16:23:39 +0100

    Avoid bundle exec

--- bin/stats_compile.rb
+++ bin/stats_compile.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 
+require 'pathname'
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path(
+  '../../Gemfile',
+  Pathname.new(__FILE__).realpath
+)
+require 'rubygems'
+require 'bundler/setup'
+
 require 'chamber'
 require 'simple_stats_store/server'
 require 'simple_stats_store/file_dump'
