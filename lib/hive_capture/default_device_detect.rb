07bde13d8bdb062baaaf018fb5e286173da6b35d
Joe Haig <joe.haig@bbc.co.uk>
2015-09-22 10:12:28 +0100

    Move ip_address and mac into device_detect

--- lib/hive_capture/default_device_detect.rb
+++ lib/hive_capture/default_device_detect.rb
@@ -12,5 +12,24 @@ class HiveCapture
       # For the moment only TVs are supported
       'tv'
     end
+
+    def ip_address
+      request.ip == '127.0.0.1' ? @env['HTTP_X_FORWARDED_FOR'].split(',')[0] : request.ip
+    end
+
+    def mac
+      `ping -c 1 #{ip_address}`
+      mac = '00:00:00:00:00:00'
+      if RUBY_PLATFORM =~ /darwin/
+        mac = `arp -n #{ip_address} | awk '{ print \$4 }'`.chomp
+      elsif RUBY_PLATFORM =~ /linux/
+        mac = `arp -an #{ip_address} | awk '{ print \$4 }'`.chomp
+      end
+
+      # Match a valid MAC address or return the default
+      /^([0-9a-fA-F]{1,2}:){5}[0-9a-fA-F]{1,2}$/ =~ mac ?
+        mac.split(/:/).map { |n| n.rjust(2, '0') }.join(':') :
+        '00:00:00:00:00:01'
+    end
   end
 end
